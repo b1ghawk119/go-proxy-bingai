9fe5aa185a0bbdf286622527504ce3d6aaa6c94a
b1ghawk119 <b1ghawk119@gmail.com>
2024-07-24 15:50:34 +0800

    Update bingapi.js

--- cloudflare/src/bingapi.js
+++ cloudflare/src/bingapi.js
@@ -827,7 +827,9 @@ export const getRandomIP = () => {
   return randomIP;
 };
 
-const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0';
+const USER_AGENT = 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_7 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.7 Mobile/15E148 Safari/605.1.15 BingSapphire/1.0.410427012';
+// const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0';
+
 
 /**
  * Get New Headers
