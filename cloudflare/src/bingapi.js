069d774ef660c9d401914f0fcf07efd21578a580
Sagsur <b1ghawk119@gmail.com>
2024-07-31 15:28:48 +0800

    Update bingapi.js

--- cloudflare/src/bingapi.js
+++ cloudflare/src/bingapi.js
@@ -5,7 +5,7 @@ const SPILT = '\x1e';
 //const SYDNEY_ORIGIN = 'https://dc.b1ng.chat';
 
 const BING_ORIGIN = 'https://b1ghawk-nbing-new.hf.space';
-const SYDNEY_ORIGIN = 'wss://b1ghawk-nbing-new.hf.space';
+const SYDNEY_ORIGIN = 'https://b1ghawk-nbing-new.hf.space';
 
 export const bingchatModel = {
   PRECISE: 'Precise',          // 精准
